de4375e506661d363bcd8fa030c4cc5bcdf858b5
jhj6879 <jo39382355@gmail.com>
2024-08-09 17:45:10 +0900

    ERP 인사 프로젝트

---
+++ src/main/resources/static/css/js/leave.js
@@ -0,0 +1,9 @@
+    function submitForm() {
+        // Form submission logic here
+        alert("휴가 신청이 저장되었습니다.");
+    }
+
+    function goBack() {
+        // Redirect back to list
+        window.location.href = 'index.html';
+    }
